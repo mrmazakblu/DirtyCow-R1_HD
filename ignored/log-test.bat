4f8daef702e8b45f9a70877c0b6c3c2a1055b4c4
mrmazakblu <mrmazakblu@gmail.com>
2017-02-26 12:50:12 -0500

    removed files that are not needed for this project

--- ignored/log-test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Files Do not match Expected && echo %date% %time% [I] Files pushed to phone do not match reference. >> "%~dp0\dirty-cow-log\log.txt"
-pause
-:error
-echo Image File not Found!! && echo %date% %time% [W] No files found in the pushed folder, Or pushed folder nor where expected. Expected location is "%~dp0 pushed" >> "%~dp0\dirty-cow-log\log.txt"
-pause
